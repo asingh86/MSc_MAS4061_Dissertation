20953a6aba6698341ad8eafd37314322b3b0813a
asingh86 <asingh86@gmail.com>
2021-05-11 05:06:27 +0100

    updating data extractor module using us giftcard data

--- extractor.py
+++ extractor.py
@@ -1,10 +1,11 @@
-import common
+from utils import common
 import os
 import pandas as pd
 import requests
 import gzip
 import random
 from typing import BinaryIO
+import csv
 
 
 class DataExtractor:
@@ -12,64 +13,44 @@ class DataExtractor:
     def __init__(self):
         self._config = common.read_configs()
 
-    def get_filepath(self) -> str:
+    def get_filepath(self) -> list:
+        """
+        :return: list of file paths
+        """
         file_name = self._config['data_url'].split("/")[-1].replace('.', "_") + '.gz'
         file_path = os.path.join(os.getcwd(), 'data/', file_name)
-        return file_path
+        header_filepath = os.path.join(os.getcwd(), self._config['files']['header'])
+        train_pos_filepath = os.path.join(os.getcwd(), self._config['files']['train_pos'])
+        train_neg_filepath = os.path.join(os.getcwd(), self._config['files']['train_neg'])
+        test_pos_filepath = os.path.join(os.getcwd(), self._config['files']['test_pos'])
+        test_neg_filepath = os.path.join(os.getcwd(), self._config['files']['test_neg'])
+        return [file_path, header_filepath, train_pos_filepath, train_neg_filepath,
+                test_pos_filepath, test_neg_filepath]
 
     def download_data(self) -> BinaryIO:
         """
-        Download data from the url
-        :return:
+        Download data from the url and save it in a file
         """
-        with open(self.get_filepath(), "wb") as f:
+        with open(self.get_filepath()[0], "wb") as f:
             r = requests.get(self._config['data_url'])
             f.write(r.content)
             f.close()
 
-    def get_samples(self, sample_size: float, seed: int) -> pd.DataFrame:
+    def count_rows(self) -> int:
         """
-        creates a dataframe of random sample from the file
-        :param sample_size: probability of selecting a sample and should be between 0 and 100
-        :param seed: This is to assign random seed for reproducability
-        :return: returns a pandas dataframe
+        Counts the number of records in a file excluding header
         """
-        if 0 < sample_size > 100:
-            raise Exception("Sample size need to be between 0 and 100")
 
-        with gzip.open(self.get_filepath(), 'rt', encoding="utf8") as f:
-            header = f.readline()
-            header = header.strip().split('\t')
-            sample_df = pd.DataFrame(columns=header)
-            random.seed(seed)
+        combined_filepath = self.get_filepath()
+        records = -1  # this is to ensure we skip header
+        with gzip.open(combined_filepath[0], 'rt', encoding="utf8") as f:
             for line in f:
-                sample = random.choices([0, 1], weights=[100 - sample_size, sample_size], k=1)
-                if sample[0] == 1:
+                records += 1
+        return records
 
-                    fields = line.split('\t')
-                    fields = pd.Series(fields, index=header)
-                    sample_df = sample_df.append(fields, ignore_index=True)
-        return sample_df
-
-
-    def get_data(self, sample_size: float = 0.01, seed: int = 123) -> pd.DataFrame():
+    def split_train_test_samples(self, sample_size: float, seed: int):
         """
-        read or download the data file
-        :param sample_size: probability of selecting a sample and should be between 0 and 100
-        :param seed: This is to assign random seed for reproducability
-        :return: returns a pandas dataframe
-        """
-
-        if os.path.exists(self.get_filepath()):
-            df = self.get_samples(sample_size, seed)
-        else:
-            self.download_data()
-            df = self.get_samples(sample_size, seed)
-        return df
-
-    def split_train_test_samples(sample_size: float, seed: int) -> pd.DataFrame:
-        """
-        creates a dataframe of random sample from the file
+        creates a csv file and stores in the location specified
         :param sample_size: probability of selecting a sample and should be between 0 and 100
         :param seed: This is to assign random seed for reproducability
         :return: returns a pandas dataframe
@@ -77,40 +58,70 @@ class DataExtractor:
         # if sample_size<0 | sample_size> 100:
         #   raise Exception("Sample size need to be between 0 and 100")
 
-        header_filepath = os.path.join(os.getcwd(), 'data/header.csv')
-        train_pos_filepath = os.path.join(os.getcwd(), 'data/train_pos.csv')
-        train_neg_filepath = os.path.join(os.getcwd(), 'data/train_neg.csv')
-        test_pos_filepath = os.path.join(os.getcwd(), 'data/test_pos.csv')
-        test_neg_filepath = os.path.join(os.getcwd(), 'data/test_neg.csv')
-        combined_filepath = [header_filepath, train_pos_filepath, train_neg_filepath, test_pos_filepath,
-                             test_neg_filepath]
+        combined_filepath = self.get_filepath()
 
-        for path in combined_filepath:
-            if os.path.exists(path):
-                os.remove(path)
+        with gzip.open(combined_filepath[0], 'rt', encoding="utf8") as f, \
+                open(combined_filepath[1], 'w') as header, \
+                open(combined_filepath[2], 'w') as train_pos, \
+                open(combined_filepath[3], 'w') as train_neg, \
+                open(combined_filepath[4], 'w') as test_pos, \
+                open(combined_filepath[5], 'w') as test_neg:
 
-        with gzip.open(filepath, 'rt', encoding="utf8") as f, \
-                open(header_filepath, 'w') as header, \
-                open(train_pos_filepath, 'w') as train_pos, \
-                open(train_neg_filepath, 'w') as train_neg, \
-                open(test_pos_filepath, 'w') as test_pos, \
-                open(test_neg_filepath, 'w') as test_neg:
+            header_writer = csv.writer(header, delimiter='\t', lineterminator='\n')
+            train_pos_writer = csv.writer(train_pos, delimiter='\t', lineterminator='\n')
+            train_neg_writer = csv.writer(train_neg, delimiter='\t', lineterminator='\n')
+            test_pos_writer = csv.writer(test_pos, delimiter='\t', lineterminator='\n')
+            test_neg_writer = csv.writer(test_neg, delimiter='\t', lineterminator='\n')
 
-            header.write(f.readline())
+            header_writer.writerow(f.readline().split('\t'))
             random.seed(seed)
+
             for line in f:
-                sample = random.choices([0, 1], weights=[100 - sample_size, sample_size], k=1)
                 fields = line.split('\t')
                 polarity = -1 if int(fields[7]) < 3 else 1 if int(fields[7]) > 3 else 0
+                sample = random.choices([0, 1], weights=[100 - sample_size, sample_size], k=1)
                 if sample[0] == 1:
                     if polarity == 1:
-                        train_pos.write(line)
+                        train_pos_writer.writerow(fields)
                     elif polarity == -1:
-                        train_neg.write(line)
+                        train_neg_writer.writerow(fields)
                 else:
                     if polarity == 1:
-                        test_pos.write(line)
+                        test_pos_writer.writerow(fields)
                     elif polarity == -1:
-                        test_neg.write(line)
+                        test_neg_writer.writerow(fields)
 
+    def get_data(self, sample_size: float = 70, seed: int = 123) -> {str: pd.DataFrame}:
+        """
+        read or download the data file
+        :param sample_size: probability of selecting a sample and should be between 0 and 100
+        :param seed: This is to assign random seed for reproducability
+        :return: returns a dictionary of sampled dataframes
+        """
 
+        combined_filepath = self.get_filepath()
+
+        if not os.path.exists(combined_filepath[0]):
+            self.download_data()
+            self.split_train_test_samples(sample_size, seed)
+        else:
+            for path in combined_filepath[1:]:
+                if not os.path.exists(path):
+                    self.split_train_test_samples(sample_size, seed)
+
+        files = {}
+        for path in combined_filepath[1:]:
+            filename = os.path.basename(path).split('.')[0]
+            files[filename] = pd.read_csv(path, header=None, sep="\t", error_bad_lines=False)
+
+        return files
+
+    def delete_data(self):
+        """
+        This method deletes any existing files in the filepath
+        """
+
+        combined_filepath = self.get_filepath()
+        for file_path in combined_filepath:
+            if os.path.exists(file_path):
+                os.remove(file_path)
--- utils/common.py
+++ utils/common.py
@@ -7,7 +7,7 @@ def read_configs() -> dict:
     This functions read config file
     :return: dict
     """
-    with open('configs/config.yml', 'r') as stream:
+    with open('/app/configs/config.yml', 'r') as stream:
         config = yaml.load(stream, Loader=yaml.SafeLoader)
     return config
 
